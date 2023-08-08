9e8a9a7918fb3389c609b4680d27e19f5101d348
Juanpav78 <juanpav.78d@gmail.com>
2023-08-08 11:58:24 -0500

    __carrousel dinamic and responsive

--- src/components/CarrouselReact.jsx
+++ src/components/CarrouselReact.jsx
@@ -3,29 +3,37 @@ import { useState } from "react"
 const CarrouselReact = ({num, children}) => {
     const [posCarrousel, setPosCarrousel] = useState(0)
     const [posIndex, setPosIndex]           = useState(0)
-
+    const [positionStyle, setPositionStyle] = useState({})
 
     const styles = {
         "--num": num
     }
+
+    const dots = (num)=>{
+        let nums = []
+        for(let i =0; i<num; i++){
+            nums.push(i)
+        }
+        return nums
+    }
     
     const handleClickArrowLeft = ()=>{
 
-        if(posCarrousel + 100/3 <= 0){
-        const carrousel = document.getElementById("carrousel")
-        carrousel.style.translate  = `${posCarrousel + 100/3}%`;
-
-        setPosCarrousel(posCarrousel + 100/3);
+        if(posCarrousel + 100/num <= 0){
+        setPositionStyle({
+                "translate": `${posCarrousel + 100/num}%`
+        })
+        setPosCarrousel(posCarrousel + 100/num);
         setPosIndex(posIndex - 1);
         }
 
     }
     const handleClickArrowRigth= ()=>{
-        if(posCarrousel - 100/3 > -100){
-            const carrousel = document.getElementById("carrousel")
-            carrousel.style.translate  = `${posCarrousel - 100/3}%`;
-    
-            setPosCarrousel(posCarrousel - 100/3);
+        if(posCarrousel - 100/num > -100){
+            setPositionStyle({
+                "translate": `${posCarrousel - 100/num}%`
+            })
+            setPosCarrousel(posCarrousel - 100/num);
             setPosIndex(posIndex + 1);
         }
 
@@ -33,22 +41,25 @@ const CarrouselReact = ({num, children}) => {
   return (
         <div className="carrousel" style={styles}   >
             <div className="carrousel__dots">
-                <span className="carrousel__dot"></span>
-                <span className="carrousel__dot active"></span>
-                <span className="carrousel__dot"></span>
+                {dots(num).map(dot =>(
+                    <span 
+                    key={dot}
+                    className={`carrousel__dot ${posIndex == dot ? "active" : ""}`}></span>
+                ))}
+            
             </div>
             <div className="carrousel__movil contain">
                 <div onClick={handleClickArrowLeft} className="carrousel__arrow carrousel__arrow--left"  id="carrousel__arrow--left">
-                <i class="fa-solid fa-arrow-left"></i>
+                <i className="fa-solid fa-arrow-left"></i>
                 </div>
                 <div className="carrousel__visible">
-                <div className="carrousel__content" id="carrousel">
+                <div style={positionStyle} className="carrousel__content">
                     {children}
                 </div>
                 </div>
                 
                 <div onClick={handleClickArrowRigth} className="carrousel__arrow carrousel__arrow--rigth" id="carrousel__arrow--rigth">
-                <i class="fa-solid fa-arrow-right"></i>
+                <i className="fa-solid fa-arrow-right"></i>
                 </div>
             </div>
            
